e4351fd33e7d27c75e1d11920185d56b7f06537f
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-06-15 23:27:31 -0300

    imc aposentadoria

---
+++ desafios-1/aposentadoria.js
@@ -0,0 +1,22 @@
+const name = "Silvana";
+const gender = "F";
+const age = 48;
+const contribution = 43;
+
+if (gender == "M"){
+    if (contribution >= 35 && contribution + age > 95){
+        console.log (`${name}, you are ready to get retared`);
+    }else{
+        console.log (`${name}, you are not ready to get retared`);
+    }
+
+}else{
+    if (contribution >= 30 && contribution + age > 85){
+        console.log (`${name}, you are ready to get retared`);
+    }else{
+        console.log (`${name}, you are not ready to get retared`);
+    }
+}
+    
+
+
---
+++ desafios-1/imc.js
@@ -0,0 +1,11 @@
+const name = "Carlos";
+const weight = 84;
+const height = 1.88;
+
+const imc = weight/ (height*height);
+
+if (imc >=30){
+    console.log (`${name}, you are over weight`)
+}else{
+    console.log (`${name}, you are not over weight`)
+}
