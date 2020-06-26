c53faba3e6037091c31c3abfdd034612824cdf8c
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-06-26 06:36:08 -0300

    converting to njk

--- fase2/modulo3/server.js
+++ fase2/modulo3/server.js
@@ -1,10 +1,28 @@
 const express = require ("express");
+const nunjucks = require ("nunjucks");
+
 const server = express();
 
+
+server.set("view engine", "html");
+server.use(express.static("public"))
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.listen(5000, function(){
-    console.log ("server is running")
+    console.log("server is running")
 })
 
 server.get("/", function(req, res){
-    return res.send("Hi")
+    return res.render("index")
+})
+
+server.get("/about", function(req, res){
+    return res.render("about")
+})
+
+server.get("/videos", function(req, res){
+    return res.render("videos")
 })
